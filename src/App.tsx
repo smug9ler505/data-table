9ea988e25a52ed2ae8a44fac04b165deadbbe9e4
iskandar <73472703+smug9ler505@users.noreply.github.com>
2026-04-16 16:50:59 +0400

    Basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useEffect, useState } from 'react'
+import { generateData, type Data } from '../utils/data/dataGenerator'
 import './App.css'
+import DataTable, { type TableColumn, type TableProps } from 'react-data-table-component'
+
+const Table = (DataTable as any).default || DataTable
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<Data[]>([])
+  useEffect(() => {
+    const fetchData = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 500))
+      const data = generateData()
+      setData(data)
+
+    }
+    fetchData()
+  }, [])
+
+  const props: TableProps<Data> = {
+    data,
+    columns,
+    pagination: true,
+    paginationRowsPerPageOptions: [10, 50, 100, 200, 500],
+    fixedHeader: true,
+    title: 'Title',
+    
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <Table
+      {...props}
+    />
   )
+
+
 }
 
 export default App
+
+
+const columns: TableColumn<Data>[] = [
+  {
+    name: 'First Name',
+    selector: row => row.first_name,
+    width: '200px'
+  },
+  {
+    name: 'Last Name',
+    selector: row => row.last_name,
+    width: '200px'
+
+  },
+  {
+    name: 'Address',
+    selector: row => row.address,
+  },
+  {
+    name: 'Phone',
+    selector: row => row.phone,
+    width: '300px'
+  },
+  {
+    name: 'Content',
+    selector: row => row.content,
+    
+  },
+  {
+    name: 'Status',
+    selector: row => row.status ? 'on' : 'off',
+    width: '100px'
+  },
+];
---
+++ utils/data/dataGenerator.ts
@@ -0,0 +1,31 @@
+import { faker } from '@faker-js/faker';
+
+
+function generateData(): Data[] {
+  const data = [];
+
+  for (let i = 1; i <= 500; i++) {
+    data.push({
+      id: i,
+      first_name: faker.person.firstName(),
+      last_name: faker.person.lastName(),
+      address: `${faker.location.streetAddress()}, ${faker.location.city()}, ${faker.location.country()}`,
+      phone: faker.phone.number(),
+      content: faker.lorem.sentences(2),
+      status: faker.datatype.boolean()
+    });
+  }
+  return data;
+}
+
+export {generateData};
+
+export interface Data {
+  id: number;
+  first_name: string;
+  last_name: string;
+  address: string;
+  phone: string;
+  content: string
+  status: boolean
+}
